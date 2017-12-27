c896c1595bbd9a105078acbc481245d3b258f24f
Christopher Dorst <chris@dorst.email>
2017-12-27 16:49:32 -0600

    Adding CommandLineToolRunner

---
+++ Common.Functions.RunCommandLineTool/CommandLineToolRunner.cs
@@ -0,0 +1,13 @@
+﻿using static Common.Functions.JoinCommandLineArguments.CommandLineArgumentsJoiner;
+using static Common.Functions.RunCommandLine.CommandLineRunner;
+
+namespace Common.Functions.RunCommandLineTool
+{
+    public static class CommandLineToolRunner
+    {
+        public static void Prompt(string directory, string tool, string command, params string[] arguments)
+            => Run(directory,
+                command: tool,
+                arguments: Join(command, arguments));
+    }
+}
